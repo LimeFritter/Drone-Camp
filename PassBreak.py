1656ed88b59d5831f8565f29346252f40e469da2
LimeFritter <derek.liebich@gmail.com>
2021-06-18 14:40:59 -0600

    finished pass break. looks visually appealing as well

--- PassBreak.py
+++ PassBreak.py
@@ -1,29 +1,35 @@
 #you can type anynumber of any length and this will guess it
-password = input("type any number")
-passLength = len(password)
-passToGuess = []
-compguess = []
-compfinal = []
-for q in range(passLength):
-    passToGuess.append(int(password[q]))
+import time
+play = True
+while play == True: 
+    password = input("type any number\n >>>>   ")
+    passLength = len(password)
+    passToGuess = []
+    compguess = []
+    compfinal = []
+    for q in range(passLength):
+        passToGuess.append(int(password[q]))
     compguess = [0] * passLength
     compfinal = [0] * passLength
 
-x = 0
-for x in range(passLength):
+    x = 0
+    for x in range(passLength):
 
-    i=0
-    for i in range (10):
-    
-        compguess[x] = i
-        print(compguess)
-        if compguess[x] == passToGuess[x]:
-            compfinal[x] = compguess[x]
+        i=0
+        for i in range (10):
             
-            
-       
-    
-            
-        
-
-print(f"{compfinal} got it")
+            time.sleep(0.1)
+            compguess[x] = i
+            print(compguess)
+            if compguess[x] == passToGuess[x]:
+                compfinal[x] = compguess[x]
+                break
+                
+    compNice = str(compfinal)
+    print(f"{compNice} got it")
+    playAgain = input("type y to play again or n to stop. \n >>>   ")
+    if playAgain == "y":
+        play = True
+    else:
+        play = False
+        print("Thanks for playing")
